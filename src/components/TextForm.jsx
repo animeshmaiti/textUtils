a70cb991d9e2b19db25659f6bf1aeca625b79314
Animesh Maiti <a.maiti770@gmail.com>
2023-12-14 12:58:48 +0530

    new utils

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Write here");
-  const handleUpClick = () => {
-    console.log("clicked upper case");
-    let newText = text.toUpperCase();
-    setText(newText);
-  };
+  const [text, inputText] = useState({
+    inText:"Write text",
+    outText: ' ',
+  });
   const handleOnChange = (event) => {
     console.log("typing...");
-    setText(event.target.value);
+    inputText({...text,inText:event.target.value,outText:""});
   };
   const readTime =(count)=>{
     let time = count*0.25;
@@ -21,7 +19,40 @@ export default function TextForm(props) {
         return `${time} seconds `;
     }
   }
-  // setText("new text")
+
+  const wordCount=()=>{
+    if(text.inText.length === 0){
+      return 0;
+    } else {
+      return text.inText.split(" ").length;
+    }
+  }
+
+
+  const handleUpClick = () => {
+    console.log("clicked upper case");
+    let newText = text.inText.toUpperCase();
+    inputText({...text,outText:newText});
+  };
+  const handleLoClick=()=>{
+    let newText=text.inText.toLocaleLowerCase();
+    inputText({...text,outText:newText})
+  }
+  const handleClearText=()=>{
+    inputText({...text,inText:"",outText:""});
+    console.log("text cleared");
+  }
+  const handleExtractEmail=()=>{
+    const reg = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g ;
+    const matches=text.inText.match(reg);
+    if(matches){
+      const emails = matches.join(",\n");
+      console.log("email extracted");
+      inputText({...text,outText:emails});
+    }else{
+      inputText({...text,outText:"Email not found"});
+    }
+  }
   return (
     <>
       <div className="container my-3">
@@ -31,20 +62,23 @@ export default function TextForm(props) {
             className="form-control"
             id="my-text"
             rows="8"
-            value={text}
+            value={text.inText}
             onChange={handleOnChange}
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
-          Convert To Uppercase
-        </button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtractEmail}>Extract Email</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear</button>
       </div>
       <div className="container my-3">
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.inText}</p>
+        <h3>Outputs</h3>
+        <p>{text.outText}</p>
         <h3>Text Summary</h3>
-        <p>{text.split(" ").length} Words and {text.length} characters</p>
-        <p>Approximate reading time {readTime(text.split(" ").length)} </p>
+        <p>{wordCount()} Words and {text.length} characters</p>
+        <p>Approximate reading time {readTime(text.inText.split(" ").length)} </p>
       </div>
     </>
   );
